1ae94a6eff63667dda0e993b62a68fb3ac5ad987
Jimmy Paulos <jpaulos@gmail.com>
2019-09-10 15:26:01 -0400

    Added train.py and run_gnn.py for training and testing automation.

--- rl_comm/run_gnn.py
+++ rl_comm/run_gnn.py
@@ -1,27 +1,78 @@
-from stable_baselines import A2C
+import numpy as np
 
-from gym_pdefense.envs.pdefense_env import PDefenseEnv
-# import gnn_policies
+def print_key_if_true(dictionary, key):
+    """
+    Print each key string whose value in dictionary is True.
+    """
+    if dictionary[key] == True:
+        return key + ', '
+    return ''
 
-env_param = {
-    'n_max_agents':3,
-    'r_capture':   0.5
-}
+def eval_pdefense_env(env, model, N, render_mode='none'):
+    """
+    Evaluate a model against an environment over N games.
+    """
+    results = {
+        'steps': np.zeros(N),
+        'score': np.zeros(N),
+        'lgr_score': np.zeros(N),
+        'initial_lgr_score': np.zeros(N)
+    }
 
-# model_name = 'test/na_3_rc_1.0_ne_16_ns_32/gnncoord_in_64-64_ag_64-64_pi__vfl__vfg_.pkl'
-model_name = 'debug/na_3_rc_0.5_ne_16_ns_32/gnnobs_in_64-64_ag_64-64_pi__vfl__vfg_.pkl'
+    for k in range(N):
+        done = False
+        obs = env.reset()
+        # Run one game.
+        while not done:
+            action, _states = model.predict(obs, deterministic=True)
+            obs, rewards, done, info = env.step(action)
+            env.render(mode=render_mode) # pick from ['none', human', 'ffmpeg']
 
-env = PDefenseEnv(
-    n_max_agents=env_param['n_max_agents'],
-    r_capture=env_param['r_capture'])
+        # Display results.
+        cause = ''.join([print_key_if_true(info, key) for key in
+            ['all_agents_dead', 'all_targets_dead', 'lgr_score_increased', 'no_more_rewards']])
+        print('{:>2} {}={}+{} {}'.format(
+            info['steps'],
+            info['initial_lgr_score'],
+            info['score'],
+            info['lgr_score'],
+            cause))
 
-model = A2C.load(model_name)
+        # Record results.
+        results['steps'][k] = info['steps']
+        results['score'][k] = info['score']
+        results['lgr_score'][k] = info['lgr_score']
+        results['initial_lgr_score'][k] = info['initial_lgr_score']
 
-while True:
-    done = False
-    obs = env.reset()
-    while not done:
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
-        env.render(mode='human') # pick from ['human', 'ffmpeg']
-    print(env.step_count)
+    print()
+    print('score,          mean = {:.1f}, std = {:.1f}'.format(np.mean(results['score']), np.std(results['score'])))
+    print('init_lgr_score, mean = {:.1f}, std = {:.1f}'.format(np.mean(results['initial_lgr_score']), np.std(results['initial_lgr_score'])))
+    return np.mean(results['score'])
+
+if __name__ == '__main__':
+
+    from stable_baselines import A2C
+    from stable_baselines.common.vec_env import SubprocVecEnv
+
+    from gym_pdefense.envs.pdefense_env_lgr import PDefenseEnv
+
+    # Specify environment.
+    env_param = {
+        'n_max_agents':      9,
+        'r_capture':         0.2,
+        'early_termination': False
+    }
+
+    env = PDefenseEnv(
+        n_max_agents=env_param['n_max_agents'],
+        r_capture=env_param['r_capture'],
+        early_termination=env_param['early_termination'])
+
+    # Specify model.
+    # model_name = 'models/2019-09-07/ppo2_lgr_9v9/na_9_rc_0.2_ne_32_ns_32/gnnfwd_in_64-64_ag__enc_64-64_msg_8_dec_64-64_ag_64-64_pi__vfl__vfg_64.pkl'
+    model_name = 'models/2019-09-10/debug/na_9_rc_0.2_ne_32_ns_32/gnnfwd_in_64-64_ag__enc_64-64_msg_8_dec_64-64_ag_64-64_pi__vfl__vfg_64.pkl'
+
+    model = A2C.load(model_name)
+
+    # Evaluate.
+    eval_pdefense_env(env, model, 100, render_mode='none')
---
+++ rl_comm/train.py
@@ -0,0 +1,272 @@
+import os.path
+import numpy as np
+import functools
+
+import tensorflow as tf
+
+from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines import PPO2
+
+from gym_pdefense.envs.pdefense_env_lgr import PDefenseEnv
+import gnn_fwd
+
+def train_param_string(p):
+    """Return identifier string for A2C training parameter dict."""
+    return 'ne_{ne}_ns_{ns}'.format(
+        ne=p['n_env'],
+        ns=p['n_steps'])
+
+def env_param_string(p):
+    """Return identifier string for PDefenseEnv environment parameter dict."""
+    return 'na_{na}_rc_{rc}'.format(
+        na=p['n_max_agents'],
+        rc=p['r_capture'])
+
+def print_key_if_true(dictionary, key):
+    """
+    Print each key string whose value in dictionary is True.
+    """
+    if dictionary[key] == True:
+        return key + ', '
+    return ''
+
+def eval_pdefense_env(env, model, N, render_mode='none'):
+    """
+    Evaluate a model against an environment over N games.
+    """
+    results = {
+        'steps': np.zeros(N),
+        'score': np.zeros(N),
+        'lgr_score': np.zeros(N),
+        'initial_lgr_score': np.zeros(N)
+    }
+
+    for k in range(N):
+        done = False
+        obs = env.reset()
+        # Run one game.
+        while not done:
+            action, _states = model.predict(obs, deterministic=True)
+            obs, rewards, done, info = env.step(action)
+            env.render(mode=render_mode) # pick from ['none', human', 'ffmpeg']
+
+        # Display results.
+        cause = ''.join([print_key_if_true(info, key) for key in
+            ['all_agents_dead', 'all_targets_dead', 'lgr_score_increased', 'no_more_rewards']])
+        print('{:>2} {}={}+{} {}'.format(
+            info['steps'],
+            info['initial_lgr_score'],
+            info['score'],
+            info['lgr_score'],
+            cause))
+
+        # Record results.
+        results['steps'][k] = info['steps']
+        results['score'][k] = info['score']
+        results['lgr_score'][k] = info['lgr_score']
+        results['initial_lgr_score'][k] = info['initial_lgr_score']
+
+    print()
+    print('score,          mean = {:.1f}, std = {:.1f}'.format(np.mean(results['score']), np.std(results['score'])))
+    print('init_lgr_score, mean = {:.1f}, std = {:.1f}'.format(np.mean(results['initial_lgr_score']), np.std(results['initial_lgr_score'])))
+    return np.mean(results['score'])
+
+def callback(locals_, globals_, test_env):
+    self_ = locals_['self']
+    if self_.num_timesteps % 20000 == 0:
+        score = eval_pdefense_env(test_env, self_, 200, render_mode='none')
+        summary = tf.Summary(value=[tf.Summary.Value(tag='score', simple_value=score)])
+        locals_['writer'].add_summary(summary, self_.num_timesteps)
+    return True
+
+def train_helper(env_param, test_env_param, train_param, policy_fn, policy_param, directory, name):
+
+    env = SubprocVecEnv([lambda: PDefenseEnv(
+        n_max_agents=env_param['n_max_agents'],
+        r_capture=env_param['r_capture'],
+        early_termination=env_param['early_termination']) for _ in range(train_param['n_env'])])
+
+    test_env = PDefenseEnv(
+        n_max_agents=test_env_param['n_max_agents'],
+        r_capture=test_env_param['r_capture'],
+        early_termination=test_env_param['early_termination'])
+
+    pkl_file = directory + '/' + name + '.pkl'
+    tensorboard_log = directory + '/' + name
+
+    if not os.path.exists(pkl_file):
+        print('Creating new model ' + pkl_file + '.')
+        model = PPO2(
+            policy=policy_fn,
+            policy_kwargs=policy_param,
+            env=env,
+            n_steps=train_param['n_steps'],
+            ent_coef=0.001,
+            verbose=1,
+            tensorboard_log=tensorboard_log,
+            full_tensorboard_log=False)
+    else:
+        print('Loading model ' + pkl_file + '.')
+        model = PPO2.load(pkl_file, env,
+            tensorboard_log=tensorboard_log)
+
+    print('Learning...')
+    model.learn(
+        total_timesteps=train_param['total_timesteps'],
+        log_interval=1000,
+        reset_num_timesteps=False,
+        callback=functools.partial(callback, test_env=test_env))
+
+    print('Saving model...')
+    model.save(pkl_file)
+
+    print('Finished.')
+
+if __name__ == '__main__':
+
+    import copy
+
+    jobs = [] # string name, policy class, policy_kwargs
+
+    # # Input feature processing with all parameters shared, msg_size = 0.
+    # j = {}
+    # j['policy'] = gnn_fwd.GnnFwd
+    # j['policy_param'] = {
+    #     'input_feat_layers':    (64,64),
+    #     'feat_agg_layers':      (64,64),
+    #     'msg_enc_layers':       (),
+    #     'msg_size':             0,
+    #     'msg_dec_layers':       (),
+    #     'msg_agg_layers':       (),
+    #     'pi_head_layers':       (),
+    #     'vf_local_head_layers': (),
+    #     'vf_global_head_layers':()}
+    # j['name'] = j['policy'].policy_param_string(j['policy_param'])
+    # jobs.append(j)
+
+    # # Input feature processing with all parameters shared, msg_size = 0.
+    # j = {}
+    # j['policy'] = gnn_fwd.GnnFwd
+    # j['policy_param'] = {
+    #     'input_feat_layers':    (64,64),
+    #     'feat_agg_layers':      (64,64),
+    #     'msg_enc_layers':       (),
+    #     'msg_size':             0,
+    #     'msg_dec_layers':       (64,),
+    #     'msg_agg_layers':       (64,),
+    #     'pi_head_layers':       (),
+    #     'vf_local_head_layers': (),
+    #     'vf_global_head_layers':()}
+    # j['name'] = j['policy'].policy_param_string(j['policy_param'])
+    # jobs.append(j)
+
+    # # Input feature processing with all parameters shared, msg_size = 8.
+    # j = {}
+    # j['policy'] = gnn_fwd.GnnFwd
+    # j['policy_param'] = {
+    #     'input_feat_layers':    (64,64),
+    #     'feat_agg_layers':      (64,64),
+    #     'msg_enc_layers':       (),
+    #     'msg_size':             8,
+    #     'msg_dec_layers':       (64,),
+    #     'msg_agg_layers':       (64,),
+    #     'pi_head_layers':       (),
+    #     'vf_local_head_layers': (),
+    #     'vf_global_head_layers':()}
+    # j['name'] = j['policy'].policy_param_string(j['policy_param'])
+    # jobs.append(j)
+
+    # Miniature ICRA 2018 with msg_size = 8.
+    # j = {}
+    # j['policy'] = gnn_fwd.GnnFwd
+    # j['policy_param'] = {
+    #     'input_feat_layers':    (64,64),
+    #     'feat_agg_layers':      (),
+    #     'msg_enc_layers':       (64,64),
+    #     'msg_size':             8,
+    #     'msg_dec_layers':       (64,64),
+    #     'msg_agg_layers':       (64,64),
+    #     'pi_head_layers':       (),
+    #     'vf_local_head_layers': (),
+    #     'vf_global_head_layers':()}
+    # j['name'] = j['policy'].policy_param_string(j['policy_param'])
+    # jobs.append(j)
+
+    # Miniature ICRA 2018 with msg_size = 0.
+    # j = {}
+    # j['policy'] = gnn_fwd.GnnFwd
+    # j['policy_param'] = {
+    #     'input_feat_layers':    (64,64),
+    #     'feat_agg_layers':      (),
+    #     'msg_enc_layers':       (64,64),
+    #     'msg_size':             0,
+    #     'msg_dec_layers':       (64,64),
+    #     'msg_agg_layers':       (64,64),
+    #     'pi_head_layers':       (),
+    #     'vf_local_head_layers': (),
+    #     'vf_global_head_layers':()}
+    # j['name'] = j['policy'].policy_param_string(j['policy_param'])
+    # jobs.append(j)
+
+    # Miniature ICRA 2018 with msg_size = 8 and global vf head.
+    j = {}
+    j['policy'] = gnn_fwd.GnnFwd
+    j['policy_param'] = {
+        'input_feat_layers':    (64,64),
+        'feat_agg_layers':      (),
+        'msg_enc_layers':       (64,64),
+        'msg_size':             8,
+        'msg_dec_layers':       (64,64),
+        'msg_agg_layers':       (64,64),
+        'pi_head_layers':       (),
+        'vf_local_head_layers': (),
+        'vf_global_head_layers':(64,)}
+    j['name'] = j['policy'].policy_param_string(j['policy_param'])
+    jobs.append(j)
+
+    # # Miniature ICRA 2018 with msg_size = 8 and global vf head.
+    # j = {}
+    # j['policy'] = gnn_fwd.GnnFwd
+    # j['policy_param'] = {
+    #     'input_feat_layers':    (128,128),
+    #     'feat_agg_layers':      (),
+    #     'msg_enc_layers':       (128,128),
+    #     'msg_size':             8,
+    #     'msg_dec_layers':       (128,128),
+    #     'msg_agg_layers':       (128,128),
+    #     'pi_head_layers':       (),
+    #     'vf_local_head_layers': (),
+    #     'vf_global_head_layers':(128,)}
+    # j['name'] = j['policy'].policy_param_string(j['policy_param'])
+    # jobs.append(j)
+
+
+    env_param = {
+        'n_max_agents':      9,
+        'r_capture':         0.2,
+        'early_termination': True
+    }
+
+    test_env_param = copy.deepcopy(env_param)
+    test_env_param['early_termination'] = False
+
+    train_param = {
+        'n_env':32,
+        'n_steps':32,
+        'total_timesteps':10000000
+    }
+
+    root = 'models/2019-09-10/9v9/'
+
+    for j in jobs:
+
+        directory = root + '/' + env_param_string(env_param) + '_' + train_param_string(train_param)
+
+        train_helper(
+            env_param       = env_param,
+            test_env_param  = test_env_param,
+            train_param     = train_param,
+            policy_fn       = j['policy'],
+            policy_param    = j['policy_param'],
+            directory       = directory,
+            name            = j['name'])
