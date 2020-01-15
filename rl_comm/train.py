72f13ce9f95dadd90f668e1b7b4e7b3cf75101af
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-01-15 11:48:13 -0500

    low lr

--- rl_comm/train.py
+++ rl_comm/train.py
@@ -108,7 +108,7 @@ def train_helper(env_param, test_env_param, train_param, policy_fn, policy_param
             policy=policy_fn,
             policy_kwargs=policy_param,
             env=env,
-            learning_rate=1e-6,
+            learning_rate=5e-6,
             cliprange=1.0,
             n_steps=train_param['n_steps'],
             ent_coef=0.001,
