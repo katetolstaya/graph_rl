6e23a1c856fbc55481e2d1a4bc47583262251fa6
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-01-18 14:09:48 -0500

    nonormalization for recroding

--- rl_comm/record_expert.py
+++ rl_comm/record_expert.py
@@ -127,3 +127,4 @@ def make_env():
 
 # env = VecNormalize(SubprocVecEnv([make_env] * 1), norm_obs=False, norm_reward=True)
 generate_expert_traj(env=make_env(), save_path='expert_rad_49', n_episodes=25)
+# generate_expert_traj(env=env, save_path='expert_rad_49', n_episodes=25)
