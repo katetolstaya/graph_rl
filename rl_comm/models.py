41bc415bafe7f4f0921b62a45990230b7473643a
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-01-17 19:21:09 -0500

    max aggregation

--- rl_comm/models.py
+++ rl_comm/models.py
@@ -71,8 +71,8 @@ class MLPGraphNetwork(snt.AbstractModule):
             self._network = modules.GraphNetwork(
                 edge_model_fn=make_mlp_model,
                 node_model_fn=make_mlp_model,
-                global_model_fn=make_mlp_model) #,
-                #reducer=unsorted_segment_max_or_zero)
+                global_model_fn=make_mlp_model,
+                reducer=unsorted_segment_max_or_zero)
 
     def _build(self, inputs):
         return self._network(inputs)
