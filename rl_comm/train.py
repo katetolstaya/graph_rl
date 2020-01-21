752420783f3d5fea324ee530d7708ed471d54aee
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-01-21 08:57:45 -0500

    larger learning rate

--- rl_comm/train.py
+++ rl_comm/train.py
@@ -122,7 +122,7 @@ def train_helper(env_param, test_env_param, train_param, policy_fn, policy_param
 
     dataset = ExpertDataset(expert_path='data/expert_rad.npz',
                             traj_limitation=-1, batch_size=128)
-    model.pretrain(dataset, n_epochs=1000, learning_rate=1e-5)
+    model.pretrain(dataset, n_epochs=1000, learning_rate=1e-4)
 
     # Training loop.
     print('\nBegin training.\n')
