741eb05adc27d5247eecca4c8552a6aeee46484a
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-02-14 17:09:52 -0500

    32 hidden units

--- rl_comm/train.py
+++ rl_comm/train.py
@@ -92,7 +92,7 @@ def main():
     j['policy'] = gnn_fwd.GnnFwd
     j['policy_param'] = {'num_processing_steps': 5}
     # j['name'] = j['policy'].policy_param_string(j['policy_param'])
-    j['name'] = 'vrp_fast'
+    j['name'] = 'vrp_fast32'
     jobs.append(j)
 
     env_name = "MappingRad-v0"
