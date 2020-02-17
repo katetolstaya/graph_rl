07885362b9b597898407ba18271b8bfcb6f0c265
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-02-17 12:05:04 -0500

    lr56

--- rl_comm/train.py
+++ rl_comm/train.py
@@ -95,7 +95,7 @@ def main():
     j['policy'] = gnn_fwd.GnnFwd
     j['policy_param'] = {'num_processing_steps': 5}
     # j['name'] = j['policy'].policy_param_string(j['policy_param'])
-    j['name'] = 'arch_lr6'
+    j['name'] = 'arch_lr56'
     jobs.append(j)
 
     env_name = "MappingRad-v0"
