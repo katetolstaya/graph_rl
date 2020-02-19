33180b6eed78c6e68633cfbdc3ead0ebe8351525
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-02-19 09:04:15 -0500

    rew7

--- rl_comm/train.py
+++ rl_comm/train.py
@@ -94,7 +94,7 @@ def main():
 
     j = {}
     j['policy'] = gnn_fwd.GnnFwd
-    j['policy_param'] = {'num_processing_steps': 5 }  #[1, 1, 2, 2, 2]}
+    j['policy_param'] = {'num_processing_steps': 5}  #[1, 1, 2, 2, 2]}
     # j['name'] = j['policy'].policy_param_string(j['policy_param'])
 
     if len(sys.argv) >= 2:
@@ -136,7 +136,7 @@ def main():
         # 'train_lr': 2e-8,
         # 'train_lr': 1e-7,
         # 'train_lr': 1e-7,
-        'train_lr': 1e-6,
+        'train_lr': 1e-7,
         'use_checkpoint': False,
     }
     # 'pretrain_dataset' = 'data/expert_rad2.npz'
