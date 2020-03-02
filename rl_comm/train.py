53b8e7b897c5994535f98edfae71a3f1532c76c7
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-03-01 19:47:53 -0500

    batch size

--- rl_comm/train.py
+++ rl_comm/train.py
@@ -65,7 +65,7 @@ def train_helper(env_param, test_env_param, train_param, policy_fn, policy_param
 
     if 'pretrain_dataset' in train_param and train_param['pretrain_dataset'] is not None:
 
-        dataset = ExpertDataset(expert_path=train_param['pretrain_dataset'], traj_limitation=-1, batch_size=10, randomize=True)
+        dataset = ExpertDataset(expert_path=train_param['pretrain_dataset'], traj_limitation=-1, batch_size=train_param['pretrain_batch'], randomize=True)
         model.pretrain(dataset, n_epochs=train_param['pretrain_epochs'], learning_rate=train_param['pretrain_lr'],
                        val_interval=10, test_env=test_env, adam_epsilon=train_param['pretrain_adam_eps'])
 
@@ -130,6 +130,7 @@ def main():
         # 'pretrain_dataset': 'data/expert_city4.npz',
         # 'pretrain_dataset': None,
         'pretrain_epochs': 600,
+        'pretrain_batch': 20,
         # 'pretrain_lr': 1e-5,
         # 'pretrain_lr': 1e-6,
         # 'pretrain_lr': 5e-7,
