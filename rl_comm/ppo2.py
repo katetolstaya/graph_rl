e65024d8b437f286253d78ffeba9bb2c5049d36d
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-06-18 11:12:30 -0400

    decaying lrs

--- rl_comm/ppo2.py
+++ rl_comm/ppo2.py
@@ -356,7 +356,7 @@ class PPO2(ActorCriticRLModel):
     def learn(self, total_timesteps, callback=None, log_interval=1, tb_log_name="PPO2",
               reset_num_timesteps=True):
         # Transform to callable if needed
-        self.learning_rate = get_schedule_fn(self.learning_rate)
+        self.learning_rate = get_schedule_fn(self.learning_rate.value)
         self.cliprange = get_schedule_fn(self.cliprange)
         cliprange_vf = get_schedule_fn(self.cliprange_vf)
 
